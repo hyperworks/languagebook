1d9b641e1f62e47d7dc0dd0d641ae20e805b2d7e
Chakrit Wichian <service@chakrit.net>
2014-08-08 00:35:43 +0700

    More robust audio handling.

--- LanguageBook/AudioController.swift
+++ LanguageBook/AudioController.swift
@@ -9,12 +9,13 @@ class AudioController: NSObject {
     private let _asset: AVAsset
     
     private var _player: AVPlayer? = nil
+    private var _playerItem: AVPlayerItem? = nil
     private var _bookmarks: [NSTimeInterval] = []
     
     var bookmarks: [NSTimeInterval] { return _bookmarks }
     
     init(audioPath: String) {
-        _asset = AVAsset.assetWithURL(NSURL.fileURLWithPath(audioPath)) as AVAsset
+        _asset = AVURLAsset(URL: NSURL.fileURLWithPath(audioPath), options: [:])
     }
     
     func addBookmark(time: NSTimeInterval) { _bookmarks.append(time) }
@@ -34,25 +35,53 @@ class AudioController: NSObject {
         
         let cmStartTime = CMTimeMakeWithSeconds(startTime, 1000000)
         let cmEndTime = CMTimeMakeWithSeconds(endTime, 1000000)
-        let duration = CMTimeMakeWithSeconds((endTime - startTime), 1000000)
-        dump(duration, name: "duration")
-        
-        let range = CMTimeRangeFromTimeToTime(cmStartTime, cmEndTime)
-        let composition = AVMutableComposition()
-        let result = composition.insertTimeRange(range,
-            ofAsset: _asset,
-            atTime: CMTimeMake(0, 1),
-            error: nil)
-        assert(result, "composition failed.")
-        
-        let item = AVPlayerItem(asset: composition.copy() as AVComposition)
-        _player = AVPlayer(playerItem: item)
-        _player?.seekToTime(CMTimeMake(0, 1))
-        _player?.play()
+
+        _playerItem = AVPlayerItem(asset: _asset)
+        _playerItem!.seekToTime(cmStartTime)
+        _playerItem!.forwardPlaybackEndTime = cmEndTime
+        _player = AVPlayer(playerItem: _playerItem)
+
+        if _playerItem!.status == .ReadyToPlay {
+            dump("playing", name: "status")
+            _player!.play()
+
+        } else {
+            dump("adding observer", name: "status")
+            _playerItem!.addObserver(self,
+                forKeyPath: "status",
+                options: NSKeyValueObservingOptions(0),
+                context: nil)
+        }
     }
-    
+
+    func itemStatusDidChange() {
+        if let pi = _playerItem? {
+            if pi.status == .ReadyToPlay {
+                dump("playing", name: "status")
+                pi.removeObserver(self, forKeyPath: "status")
+                _player?.play()
+            }
+        }
+    }
+
+
     func stop() {
+        _playerItem = nil
         _player?.pause()
         _player = nil
+    }
+
+
+    override func observeValueForKeyPath(keyPath: String!,
+        ofObject object: AnyObject!,
+        change: [NSObject : AnyObject]!,
+        context: UnsafeMutablePointer<()>) {
+
+        let item = object as? AVPlayerItem
+        if item == _playerItem && keyPath == "status" {
+            itemStatusDidChange()
+        } else {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+        }
     }
 }
