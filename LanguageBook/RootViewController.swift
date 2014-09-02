edb5c64fd608ae704a37a71b99e5c3798c4de186
Chakrit Wichian <service@chakrit.net>
2014-09-03 02:04:04 +0700

    More rotation-robust constraint handling in root view.

--- LanguageBook/RootViewController.swift
+++ LanguageBook/RootViewController.swift
@@ -2,24 +2,34 @@ import UIKit
 
 class RootViewController: UIViewController {
     let book = Book.fromJSON()
+
+    override func supportedInterfaceOrientations() -> Int {
+        var mask: UIInterfaceOrientationMask = .Portrait | .PortraitUpsideDown |
+            .LandscapeLeft | .LandscapeRight
+        return Int(mask.toRaw())
+    }
     
     override func loadView() {
-        let bounds = UIScreen.mainScreen().bounds
-        let v = UIView(frame: bounds)
+        let v = UIView(frame: CGRect(origin: CGPointZero, size: UIScreen.rotatedSize()))
         v.backgroundColor = .whiteColor()
-        
-        // TODO: Load first chapter view.
+
+        // Load first chapter, if there are multiple chapters, the first one should acts as a menu.
+        // TODO: Multi-chapter support + ability to navigate between them.
         let chapterController = ChapterViewController(book: book, chapter: book.chapters[0])
-        let chapterView = chapterController.view
         addChildViewController(chapterController)
-        
+
+        let chapterView = chapterController.view
+        chapterView.setTranslatesAutoresizingMaskIntoConstraints(false)
+
         v.addSubview(chapterView)
-        chapterView.addConstraint(NSLayoutConstraint(item: chapterView, width: bounds.size.width))
-        chapterView.addConstraint(NSLayoutConstraint(item: chapterView, height: bounds.size.height))
         v.addConstraint(NSLayoutConstraint(verticalAlignItem: chapterView, withItem: v))
         v.addConstraint(NSLayoutConstraint(horizontalAlignItem: chapterView, withItem: v))
+        v.addConstraint(NSLayoutConstraint(sizeItem: chapterView, widthEqualsToItem: v))
+        v.addConstraint(NSLayoutConstraint(sizeItem: chapterView, heightEqualsToItem: v))
         chapterController.didMoveToParentViewController(self)
-        
+
+        v.setTranslatesAutoresizingMaskIntoConstraints(true)
+        v.autoresizingMask = .FlexibleWidth | .FlexibleHeight
         view = v
     }
 }
